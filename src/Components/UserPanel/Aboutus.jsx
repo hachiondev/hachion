4e8fb5fa302387c0eaa59366a60dda5edcdf727c
Monikacoder <coolmonika.1506@gmail.com>
2024-10-03 22:43:31 +0530

    3/10

--- src/Components/UserPanel/Aboutus.jsx
+++ src/Components/UserPanel/Aboutus.jsx
@@ -87,42 +87,39 @@ Our Real-time experts with over 10 years of experience in IT are ready to take y
           <p className='about-us-left-content'>
             Hachion offers you the flexibility to learn anytime, anywhere with instructor-led online training programs...
           </p>
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
-            <div className='about-us-div-content'>
-              <img src={FlexibleLearning} alt='' />
-              <h6>Flexible Learning</h6>
-              <p>Online Training accessible at your convenience</p>
-            </div>
-            <div className='about-us-div-content'>
-              <img src={onlineTeaching} alt='' />
-              <h6>Expert Instructor</h6>
-              <p>Industries professionals delivering valueable content</p>
-            </div>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
-            <div className='about-us-div-content'>
-              <img src={handsOnLearning} alt='' />
-              <h6>Hands-On Learning</h6>
-              <p>Practical classes with easy-to-follow material.</p>
-            </div>
-            <div className='about-us-div-content'>
-              <img src={careerSupport} alt='' />
-              <h6>Career Support</h6>
-              <p>Assistance with CVs, interviews, and job placement.</p>
-            </div>
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
-            <div className='about-us-div-content'>
-              <img src={Assistance} alt='' />
-              <h6>24/7 support</h6>
-              <p>Continuous support through online chat and phone.</p>
-            </div>
-            <div className='about-us-div-content'>
-              <img src={Success} alt='' />
-              <h6>Proven Success</h6>
-              <p>Positive feedback from participants and successful outcomes.</p>
-            </div>
-          </div>
+          <div className='about-us-div'>
+  <div className='about-us-div-content'>
+    <img src={FlexibleLearning} alt='' />
+    <h6>Flexible Learning</h6>
+    <p>Online Training accessible at your convenience</p>
+  </div>
+  <div className='about-us-div-content'>
+    <img src={onlineTeaching} alt='' />
+    <h6>Expert Instructor</h6>
+    <p>Industries professionals delivering valuable content</p>
+  </div>
+  <div className='about-us-div-content'>
+    <img src={handsOnLearning} alt='' />
+    <h6>Hands-On Learning</h6>
+    <p>Practical classes with easy-to-follow material.</p>
+  </div>
+  <div className='about-us-div-content'>
+    <img src={careerSupport} alt='' />
+    <h6>Career Support</h6>
+    <p>Assistance with CVs, interviews, and job placement.</p>
+  </div>
+  <div className='about-us-div-content'>
+    <img src={Assistance} alt='' />
+    <h6>24/7 Support</h6>
+    <p>Continuous support through online chat and phone.</p>
+  </div>
+  <div className='about-us-div-content'>
+    <img src={Success} alt='' />
+    <h6>Proven Success</h6>
+    <p>Positive feedback from participants and successful outcomes.</p>
+  </div>
+</div>
+
         </div>
       </div>
       <Learners />

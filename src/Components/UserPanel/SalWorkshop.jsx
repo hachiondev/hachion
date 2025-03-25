d5d971996932d23498aed84cb1d3a0f92f07a7ff
SirishaUI <sirishahachion@gmail.com>
2025-03-25 17:44:45 +0530

    sirisha 25Mar

--- src/Components/UserPanel/SalWorkshop.jsx
+++ src/Components/UserPanel/SalWorkshop.jsx
@@ -196,30 +196,30 @@ const SalWorkshop = () => {
             <div className='workshop-content'>
           <h2 className='workshop-heading'>Program Highlights</h2>
           <div className='workshop-top-img'>
-            <div className='about-us-div-content'>
+            <div className='workshop-div-content'>
                 <img className='workshop-img' src={Exp} alt='' />
                 <h6>Expert Guidance</h6>
               </div>
-              <div className='about-us-div-content'>
+              <div className='workshop-div-content'>
                 <img className='workshop-img' src={Assig} alt='' />
                 <h6>Assignment Practices</h6>
               </div>
-              <div className='about-us-div-content'>
+              <div className='workshop-div-content'>
                 <img className='workshop-img' src={Handexp} alt='' />
                 <h6>Hands on Projects</h6>
               </div>
             </div>
 
             <div className='workshop-top-img'>
-            <div className='about-us-div-content'>
+            <div className='workshop-div-content'>
                 <img className='workshop-img' src={cv} alt='' />
                 <h6>Resume Building</h6>
               </div>
-              <div className='about-us-div-content'>
+              <div className='workshop-div-content'>
                 <img className='workshop-img' src={inter} alt='' />
                 <h6>Interview Preparation</h6>
               </div>
-              <div className='about-us-div-content'>
+              <div className='workshop-div-content'>
                 <img className='workshop-img' src={support} alt='' />
                 <h6>24/7 Support</h6>
               </div>
@@ -235,7 +235,7 @@ const SalWorkshop = () => {
         <div className='workshopform' ref={workshopRef}>
         <div className='workshop-content'>
           <h2 className='workshop-reg'>Join the Workshop Now!</h2>
-          <div className='workshop-top-img'>
+          <div className='workshop-top-form'>
           <img className='workshop-reg-img' src={salreg} alt='' />
 
           <div>

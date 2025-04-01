18c928c1f5101afcc582e1a96e4f21c086cf7253
Nagalakshmi-Insignia <nagalakshmi@insigniaconsultancy.com>
2025-04-01 23:28:00 +0530

    adding schedules

--- src/main/java/com/hachionUserDashboard/repository/CourseScheduleRepository.java
+++ src/main/java/com/hachionUserDashboard/repository/CourseScheduleRepository.java
@@ -1,12 +1,22 @@
 package com.hachionUserDashboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hachionUserDashboard.entity.CourseSchedule;
 
+import jakarta.transaction.Transactional;
+
 
 @Repository
 public interface CourseScheduleRepository extends JpaRepository <CourseSchedule,Integer>{
+
+	@Modifying
+	@Transactional
+	@Query("DELETE FROM CourseSchedule cs WHERE cs.schedule_date < :todayDateInUtc")
+	int deletePastWorkshops(@Param("todayDateInUtc") String todayDateInUtc);
 
 }

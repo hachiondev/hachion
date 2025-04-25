dcb18bda2f295223095b83910d8d3bf34c4503b5
Nagalakshmi-Insignia <nagalakshmi@insigniaconsultancy.com>
2025-04-25 21:53:12 +0530

    Lakshmi 25/04

--- src/main/java/com/hachionUserDashboard/repository/FaqRepository.java
+++ src/main/java/com/hachionUserDashboard/repository/FaqRepository.java
@@ -1,6 +1,10 @@
 package com.hachionUserDashboard.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hachionUserDashboard.entity.Faq;
@@ -8,5 +12,8 @@ import com.hachionUserDashboard.entity.Faq;
 
 @Repository
 public interface FaqRepository extends JpaRepository <Faq,Integer>{
+
+	@Query(value = "SELECT * FROM faq WHERE faq_pdf = :faqPdf", nativeQuery = true)
+	Optional<Faq> findPdfByExactName(@Param("faqPdf") String faqPdf);
 
 }

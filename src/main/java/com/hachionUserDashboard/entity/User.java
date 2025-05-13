0cff28c8aff82bf23db744d3e83bb4cd27d1a4fd
NagalakshmiHachion <nagalakshmi.hachion@gmail.com>
2025-05-13 16:29:29 +0530

    Lakshmi 13/05

--- src/main/java/com/hachionUserDashboard/entity/User.java
+++ src/main/java/com/hachionUserDashboard/entity/User.java
@@ -147,13 +147,13 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "first_name", nullable = false)
+//	@Column(name = "first_name", nullable = false)
 	private String firstName;
 	
-	@Column(name = "last_name", nullable = false)
+//	@Column(name = "last_name", nullable = false)
 	private String lastName;
 	
-    @Column(name = "user_name", nullable = false)
+//    @Column(name = "user_name", nullable = false)
 	private String userName;
 
 	@Column(name = "email", nullable = false, unique = true)

936dad7959904f67484570019b7aa52e051463ef
Nagalakshmi-Insignia <nagalakshmi@insigniaconsultancy.com>
2025-05-27 16:31:16 +0530

    Lakshmi 27/05

--- src/main/java/com/hachionUserDashboard/service/EmailService.java
+++ src/main/java/com/hachionUserDashboard/service/EmailService.java
@@ -1,4 +1,5 @@
 package com.hachionUserDashboard.service;
+
 import java.io.File;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,48 +16,51 @@ import jakarta.mail.internet.MimeMessage;
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+	@Autowired
+	private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        message.setFrom("hachion.trainings@gmail.com");
+	public void sendEmail(String to, String subject, String text) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(text);
+		message.setFrom("hachion.trainings@gmail.com");
 
-        mailSender.send(message);
-    }
-    public void sendEmailWithAttachment(String to, byte[] attachmentBytes, String subject, String body)
-            throws MessagingException {
-        
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(body);
-        helper.addAttachment("Certificate.pdf", new ByteArrayResource(attachmentBytes));
+		mailSender.send(message);
+	}
 
-        mailSender.send(message);
-    }
-    public void sendInvoiceEmail(String toEmail, String studentName, String invoicePath) {
-        try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+	public void sendEmailWithAttachment(String to, byte[] attachmentBytes, String subject, String body)
+			throws MessagingException {
 
-            helper.setTo(toEmail);
-            helper.setSubject("Invoice from Hachion");
-            helper.setText("Dear " + studentName + ",\n\nPlease find your invoice attached.\n\nRegards,\nHachion");
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-            FileSystemResource file = new FileSystemResource(new File(invoicePath));
-            helper.addAttachment("Invoice.pdf", file);
+		helper.setTo(to);
+		helper.setSubject(subject);
+		helper.setText(body);
+		helper.addAttachment("Certificate.pdf", new ByteArrayResource(attachmentBytes));
 
-            mailSender.send(message);
-            System.out.println("Invoice email sent to: " + toEmail);
-        } catch (MessagingException e) {
-            System.err.println("Failed to send email: " + e.getMessage());
-            throw new RuntimeException("Email sending failed", e);
-        }
-    }
+		mailSender.send(message);
+	}
+
+	public void sendInvoiceEmail(String toEmail, String studentName, String invoicePath) {
+		try {
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+			helper.setTo(toEmail);
+			helper.setSubject("Invoice from Hachion");
+			helper.setText("Dear " + studentName + ",\n\nPlease find your invoice attached.\n\nRegards,\nHachion");
+			helper.setCc("hachion.trainings@gmail.com");
+
+			FileSystemResource file = new FileSystemResource(new File(invoicePath));
+			helper.addAttachment("Invoice.pdf", file);
+
+			mailSender.send(message);
+			System.out.println("Invoice email sent to: " + toEmail);
+		} catch (MessagingException e) {
+			System.err.println("Failed to send email: " + e.getMessage());
+			throw new RuntimeException("Email sending failed", e);
+		}
+	}
 }

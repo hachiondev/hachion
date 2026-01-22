89977cdf35474ba95e169803b8b7322ed4ccb016
suyash srivastava <suyash.hachion@gmail.com>
2026-01-22 15:48:31 +0530

    Update NewCourseDetails.jsx

--- src/Components/UserPanel/NewcoursePage/NewCourseDetails.jsx
+++ src/Components/UserPanel/NewcoursePage/NewCourseDetails.jsx
@@ -8,6 +8,7 @@ import FinalCTA from '../NewcoursePage/components/FinalCTA';
 import InstructorSection from '../NewcoursePage/components/InstructorSection';
 import LearnSection from '../NewcoursePage/components/LearnSection';
 import StudentsAlsoEnrolled from '../NewcoursePage/components/StudentsAlsoEnrolled';
+import TrendingCourseNames from '../CoursePage/components/TrendingCourseNames'
 import StudentsSay from '../NewcoursePage/components/StudentsSay';
 import SuccessStories from '../NewcoursePage/components/SuccessStories';
 import { useEffect, useRef } from 'react';
@@ -49,6 +50,7 @@ const NewCourseDetails = () => {
         onChat={() => console.log("Open chat widget")}
         onSchedule={() => console.log("Open scheduler")}
       />
+      <TrendingCourseNames/>
       {/* <FinalCTA
         onEnroll={() => console.log("Enroll clicked")}
         onAddToCart={() => console.log("Add to cart")}
